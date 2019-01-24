181de7712df839dcd0ad88bb615c9c1138a604c6
s-mawjee <21179950+s-mawjee@users.noreply.github.com>
2019-01-24 17:36:06 +0200

    Linear Programming approach to IRL

--- utils.py
+++ utils.py
@@ -2,24 +2,24 @@ import numpy as np
 import math
 from collections import namedtuple
 
-Step = namedtuple('Step', 'cur_state action next_state reward done')
+Step = namedtuple('Step','cur_state action next_state reward done')
 
 
 def normalize(vals):
-    """
-    normalize to (0, max_val)
-    input:
-      vals: 1d array
-    """
-    min_val = np.min(vals)
-    max_val = np.max(vals)
-    return (vals - min_val) / (max_val - min_val)
+  """
+  normalize to (0, max_val)
+  input:
+    vals: 1d array
+  """
+  min_val = np.min(vals)
+  max_val = np.max(vals)
+  return (vals - min_val) / (max_val - min_val)
 
 
 def sigmoid(xs):
-    """
-    sigmoid function
-    inputs:
-      xs      1d array
-    """
-    return [1 / (1 + math.exp(-x)) for x in xs]
+  """
+  sigmoid function
+  inputs:
+    xs      1d array
+  """
+  return [1 / (1 + math.exp(-x)) for x in xs]
